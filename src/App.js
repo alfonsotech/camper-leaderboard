7d1ad800cbc5027430560b58616420a0525d8e9a
alfonsotech <alfonsotech@nym.hush.com>
2017-12-20 16:15:23 -0600

    Adds axios call to API endpoint and uses data to set state of App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      recentCampers: [],
+      allTimeCampers: [],
+      currentView: 'recentCampers'
+    }
+    // this.componentWillMount = this.componentWillMount.bind(this)
+    // this.fetchRecentCampers = this.fetchRecentCampers.bind(this)
+    // this.fetchAllTimeCampers = this.fetchAllTimeCampers.bind(this)
+  }
+
+  //this function will run once before the first render of the component
+  //a good place to fetch from API
+  componentWillMount() {
+    //make concurrent requests and set state to response
+    axios.all([this.fetchRecentCampers(), this.fetchAllTimeCampers()])
+  .then(axios.spread(function (recentCampers, allTimeCampers) {
+    // Both requests are now complete
+    this.setState({ recentCampers:recentCampers, allTimeCampers:allTimeCampers })
+  }))
+
+
+
+    // axios.all([this.fetchRecentCampers(), this.fetchAllTimeCampers()])
+    // .then(axios.spread(function(recentCampers, allTimeCampers) {
+    //   this.setState({ recentCampers:recentCampers, allTimeCampers:allTimeCampers })
+    // }))
+    // this.fetchRecentCampers();
+    // this.fetchAllTimeCampers();
+  }
+
+  fetchRecentCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    // .then(function(recentCampers) {
+    //   this.setState({ recentCampers:recentCampers })
+    // })
+  }
+
+  fetchAllTimeCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+    // .then(function(allTimeCampers) {
+    //   this.setState({ allTimeCampers:allTimeCampers })
+    // })
+  }
+
+
+
   render() {
     return (
       <div className="App">
